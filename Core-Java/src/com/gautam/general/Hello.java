1823c1383385971c89aebf03cc968324af96e084
Gautam Kumar <gautam14cs@gmail.com>
2024-08-10 00:06:08 +0530

    Core Java Repositoy created

---
+++ Core-Java/src/com/gautam/general/Hello.java
@@ -0,0 +1,12 @@
+package com.gautam.general;
+
+import java.util.Arrays;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println(" Hello World !!");
+        System.out.println(" Welcome to Core Java Repository ............ ");
+
+        System.out.println(Arrays.toString(new int[] {1,2,3,4,5,6,7,8,9,10}));
+    }
+}
